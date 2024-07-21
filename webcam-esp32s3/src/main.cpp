e18e41755b16facf9bc69a7d54515d23000fc74b
James Rutter <jamesdavidrutter@gmail.com>
2024-07-21 08:47:03 -0400

    improve framesize to HD and remove extraneous conditionals

--- webcam-esp32s3/src/main.cpp
+++ webcam-esp32s3/src/main.cpp
@@ -47,7 +47,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 
 void setup()
 {
-  Serial.begin(115200);
+  // Serial.begin(115200);
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -71,70 +71,33 @@ void setup()
   config.xclk_freq_hz = 20000000;
   config.frame_size = FRAMESIZE_UXGA;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.grab_mode = CAMERA_GRAB_LATEST;
   config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    if (psramFound())
-    {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    }
-    else
-    {
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  }
-  else
-  {
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK)
   {
-    Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
 
   sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID)
-  {
-    s->set_vflip(s, 1);       // flip it back
-    s->set_brightness(s, 1);  // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
+
+  s->set_framesize(s, FRAMESIZE_HD);
 
   WiFi.begin(ssid, password);
   WiFi.setSleep(false);
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-    Serial.print(".");
+    delay(250);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
 
   // Set up WebSocket Connection
-  Serial.printf("Connecting to the WebSocket Server...\n");
-  // websocket.beginSSL("maker.deno.dev", 443, "/ws/esp32");  // PRODUCTION
-  websocket.begin(local_ws_server, 8000, "/ws/esp32"); // LOCAL
+  websocket.beginSSL("maker.deno.dev", 443, "/ws/esp32"); // PRODUCTION
+  // websocket.begin(local_ws_server, 8000, "/ws/esp32"); // LOCAL
   websocket.onEvent(webSocketEvent);
   websocket.setReconnectInterval(5000);
 }
@@ -147,7 +110,6 @@ void loop()
   camera_fb_t *fb = esp_camera_fb_get();
   if (!fb)
   {
-    Serial.println("Camera capture failed");
     esp_camera_fb_return(fb);
     return;
   }
